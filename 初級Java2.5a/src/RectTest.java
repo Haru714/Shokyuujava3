0024d6cf5e6e93fd54f130ea8a125ebee0a8962a
haru <zeal23310023@fsgcl.jp>
2024-05-14 12:12:54 +0900

    java 3.3

---
+++ 初級Java2.5a/src/RectTest.java
@@ -0,0 +1,16 @@
+
+public class RectTest extends MyFrame{
+	public void run() {
+		setColor(0,0,0);
+		fillRect(30,40,50,100);
+		setColor(255,255,0);
+		fillRect(130,40,50,100);
+		setColor(200,200,200);
+		fillRect(30,200,150,10);
+		setColor(0,250,255);
+		fillRect(90,160,30,30);
+		setColor(150,50,200);
+		fillRect(60,160,30,30);
+		
+	}
+}
