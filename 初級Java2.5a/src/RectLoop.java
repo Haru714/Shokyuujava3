0681c4b2326ca76fc46c634e4e408c54ecff9b19
haru <zeal23310023@fsgcl.jp>
2024-05-21 10:11:12 +0900

    確認問題４－4

--- 初級Java2.5a/src/RectLoop.java
+++ 初級Java2.5a/src/RectLoop.java
@@ -2,8 +2,9 @@
 public class RectLoop extends MyFrame {
 	public void rum() {
 		setColor(0,128,0);
-		int x=30;
-		while (x<=170) {
+       int x=30;
+		for(int i= 30; i <15; i++) {
+			System.out.println(x);
 			fillRect(x,80,10,100);
 			x+=20;
 		}
--- 初級Java2.5a/src/TextTestTest.java
+++ 初級Java2.5a/src/TextTestTest.java
@@ -2,8 +2,8 @@
 public class TextTestTest extends MyFrame{
 	
 	public void run() {
-		drawString("おはよう",100,15)
-		drawString("こんにちは",100,200,20)
+		drawString("おはよう",100,100,15);
+		drawString("こんにちは",100,200,20);
 	}
 		
 
