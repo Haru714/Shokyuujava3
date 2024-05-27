e3ad4076d01f261329bbaa805bcfd72b47a1a5be
haru <zeal23310023@fsgcl.jp>
2024-05-27 11:18:44 +0900

    練習問題5-2

--- 初級Java2.5a/src/DisplayRectsSlow.java
+++ 初級Java2.5a/src/DisplayRectsSlow.java
@@ -2,26 +2,38 @@
 public class DisplayRectsSlow extends MyFrame{
 	public void run() {
 	int  y=100;
-	int x=30;
-	while(true) {
+	int x=100;
 	
-	while(y>=30) {
+		while (x<=300){
+	    	//(1)
+	    	clear();
+	    	//(2)
+	    setColor(0,128,0);
+	   	fillRect(x,y,10,100);
+	   	x+=5;	
+	   	y+=5;
+	sleep(0.1);
+		}
+		while (x>=100){
+	    	//(1)
+	    	clear();
+	    	//(2)
+	    	setColor(0,128,0);
+	    	fillRect(x,y,10,100);
+	    	x-=5;		
+					sleep(0.1);
+	    }
+	while(x<=300) {
 		clear();
 	setColor(0,128,0);
 	fillRect(x,y,10,100);
+	x+=5;
 	y-=5;
 	sleep(0.1);
 	}
 	
-	while (x<=170){
-    	//(1)
-    	clear();
-    	//(2)
-    	setColor(0,128,0);
-    	fillRect(x,y,10,100);
-    	x+=5;		
-				sleep(0.1);
-    }
+	
+    
   }
  }
-	}
+	
