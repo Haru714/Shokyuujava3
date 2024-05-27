6a1102bb32c56039c4869e80ff15db5011b430da
haru <zeal23310023@fsgcl.jp>
2024-05-27 09:41:13 +0900

    no message

---
+++ 初級Java2.5/src/start.java
@@ -0,0 +1,26 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+/*MyFrame frame1=new MyFrame();
+frame1.setLocation(200,100);
+frame1.setSize(150,150);
+MyFrame frame2=new MyFrame();
+frame2.setLocation(350,200);
+*/
+MyFrame frame3=new レクトフレーム();
+frame3.setLocation(50,300);
+
+int x=2;
+int y=3;
+System.out.println(x*y);
+
+MyFrame frame4=new レクトフレーム();
+frame4.setLocation(50,30);
+MyFrame frame5=new レクトフレーム();
+frame5.setLocation(70,100);
+	}
+	
+
+}
