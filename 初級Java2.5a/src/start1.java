49e941620c90eaef1cf157d2b0bad6406225f66a
haru <zeal23310023@fsgcl.jp>
2024-05-28 10:44:43 +0900

    java 6,4

--- 初級Java2.5a/src/start1.java
+++ 初級Java2.5a/src/start1.java
@@ -3,8 +3,8 @@ public class start1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-System.out.println("プログラムを実行する");
-MyFrame frame1=new OvalTest();
+		System.out.println("プログラムを実行する");
+		MyFrame frame1=new DisplayFaceObject();
 
 	}
 
